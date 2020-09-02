8d1123bd0a0c8f9b8aa351b11b918c40895003a2
Bohdan Kabay <kabaybodj@gmail.com>
2020-09-02 20:02:16 +0300

    Fix errors

---
+++ precache-manifest.4b9362b20adb4648bba6ec694fcbd853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21d44b7a21b4ef055a20d2d8e093531",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c17f036ae571925be68c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a3ef2ffb69239c37d65f",
+    "url": "/static/js/2.cfe68936.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.cfe68936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c17f036ae571925be68c",
+    "url": "/static/js/main.f25cc3cd.chunk.js"
+  },
+  {
+    "revision": "3c58dc67cdf590102501",
+    "url": "/static/js/runtime-main.a82df0ca.js"
+  }
+]);
