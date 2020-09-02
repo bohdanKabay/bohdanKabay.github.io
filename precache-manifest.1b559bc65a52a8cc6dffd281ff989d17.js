91991fb497150ef0937f8343bced637f6ada1b77
Bohdan Kabay <kabaybodj@gmail.com>
2020-09-02 18:01:15 +0300

    Initial commit

---
+++ precache-manifest.1b559bc65a52a8cc6dffd281ff989d17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ceddc958e5e15861e70347ba705f6d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff299959bf5bfd5f27ad",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "766c071b6e005bbdb8b7",
+    "url": "/static/js/2.d112d2e4.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.d112d2e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff299959bf5bfd5f27ad",
+    "url": "/static/js/main.f74024f9.chunk.js"
+  },
+  {
+    "revision": "3c58dc67cdf590102501",
+    "url": "/static/js/runtime-main.a82df0ca.js"
+  }
+]);
